9fa4f8eebcffa97e4857301b83904082798c5a65
Andrea Megias Muñoz <168994062+Andremegi@users.noreply.github.com>
2025-09-03 10:25:48 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+let pointsHome = document.getElementById("points_home")
+let pointGuest = document.getElementById("points_guest")
+
+let count = 0
+let countg =0
+
+pointsHome.innerText=count
+pointsGuest.innerText=countg
+
+
+function plus1(){
+    count +=1
+    pointsHome.innerText=count
+
+}
+function plus2(){
+    count +=2
+    pointsHome.innerText=count
+
+}
+function plus3(){
+    count +=3
+    pointsHome.innerText=count
+
+}
+function plus1g(){
+    countg +=1
+    pointsGuest.innerText=count
+
+}
+function plus2g(){
+    countg +=2
+    pointsGuest.innerText=count
+
+}
+function plus3g(){
+    countg +=3
+    pointsGuest.innerText=count
+
+}
+function newgame(){
+    count = 0
+    pointsHome.innerText=count
+
+    countg =0
+    pointsGuest.innerText=count
+}
